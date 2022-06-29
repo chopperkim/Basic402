916bc49998b20a63e450cf78812dc096544e8936
chopperkim <chocojhkim@gmail.com>
2022-06-29 12:49:21 +0900

    chapter 08

---
+++ src/chapter08/DriverExample.java
@@ -0,0 +1,17 @@
+package chapter08;
+
+public class DriverExample {
+	public static void main(String[] args) {
+		Driver driver = new Driver();
+		Vehicle vehicle = new Taxi();
+		if (vehicle instanceof Bus) {
+			Bus bus2 = (Bus) vehicle;
+		}
+		
+		vehicle = new Bus();
+		Bus bus = (Bus) vehicle;
+		
+		driver.drive(new Bus());
+		driver.drive(new Taxi());
+	}
+}
