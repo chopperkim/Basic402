ebc28253044da354964650837cbd62bdd492db74
chopperkim <chocojhkim@gmail.com>
2022-07-08 11:30:07 +0900

    Stream API 적용

--- src/chapter13/confirm/MapExample.java
+++ src/chapter13/confirm/MapExample.java
@@ -1,8 +1,10 @@
 package chapter13.confirm;
 
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
@@ -13,7 +15,7 @@ public class MapExample {
 		map.put("blue", 96);
 		map.put("hong", 86);
 		map.put("white", 92);
-		
+		/*
 		String name = null;
 		int maxScore = 0;
 		int totalScore = 0;
@@ -26,6 +28,14 @@ public class MapExample {
 				name = entry.getKey();
 			}
 		}
+		System.out.println("평균점수: " + (totalScore / map.size()));
+		System.out.println("최고점수: " + maxScore);
+		System.out.println("최고점수를 받은 아이디: " + name);
+		*/
+		Entry<String, Integer> maxEntry = map.entrySet().stream().max(Comparator.comparingInt(Entry<String, Integer>::getValue)).get();
+		String name = maxEntry.getKey();
+		int maxScore = maxEntry.getValue();
+		int totalScore = map.entrySet().stream().mapToInt(value -> value.getValue()).sum();
 		System.out.println("평균점수: " + (totalScore / map.size()));
 		System.out.println("최고점수: " + maxScore);
 		System.out.println("최고점수를 받은 아이디: " + name);
